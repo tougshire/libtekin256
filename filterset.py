a419ef77a7a3de9ad1c65f6468bb6596ad5992d3
tougshire <tougshire@tougshire.com>
2025-07-17 09:13:52 -0400

    Add script to disable retrieve button

--- filterset.py
+++ filterset.py
@@ -2,17 +2,20 @@ import django_filters
 from django_filters_stoex.filterset import StoexFilterSet
 from .models import (
     Article,
+    ArticleCategory,
     ArticleStatus,
-    Mamodel
+    Mamodel,
+    MamodelCategory
 )
 from django.db import models
 from django import forms
 from django_filters_stoex.filters import ChainableOrderingFilter, CrossFieldSearchFilter
-from touglates.widgets import DropdownSelectMultiple
+from touglates.widgets import ClearableTextInput, DropdownSelectMultiple
 
 class ArticleFilter(StoexFilterSet):
 
     filterset_common_name = forms.CharField()
+
     combined_text_search = CrossFieldSearchFilter(
         label="Combined Text Search(Name, Model, Index, etc..)",
         field_name="common_name, inventory_index, mamodel__brand, mamodel__name, mamodel__number, assignee__fullformalname, assignee__friendlyname, customfield01, customfield02, customfield03, customfield04, customfield05, customfield06, customfield07, customfield08, customfield09, customfield10, customfield11, customfield12, customfield13, customfield14, customfield15, customfield16",
@@ -20,10 +23,11 @@ class ArticleFilter(StoexFilterSet):
         help_text="General Text in the Article Record",
     )
     common_name = django_filters.CharFilter(
-        label="Combined Text Search(Name, Model, Index, etc..)",
-        field_name="common_name, inventory_index, mamodel__brand, mamodel__name, mamodel__number, assignee__fullformalname, assignee__friendlyname, customfield01, customfield02, customfield03, customfield04, customfield05, customfield06, customfield07, customfield08, customfield09, customfield10, customfield11, customfield12, customfield13, customfield14, customfield15, customfield16",
+        label="Common Name",
+        field_name="common_name",
         lookup_expr="icontains",
-        help_text="General Text in the Article Record",
+        help_text="Common Name",
+        widget=ClearableTextInput(),
     )
 
     mamodel__in = django_filters.ModelMultipleChoiceFilter(
@@ -31,6 +35,22 @@ class ArticleFilter(StoexFilterSet):
         field_name="mamodel",
         queryset=Mamodel.objects.all(),
         help_text="Model",
+        widget=DropdownSelectMultiple(),
+    )
+
+    mamodel__category__in = django_filters.ModelMultipleChoiceFilter(
+        label="Model Category",
+        field_name="mamodel__category",
+        queryset=MamodelCategory.objects.all(),
+        help_text="Model",
+        widget=DropdownSelectMultiple(),
+    )
+
+    category__in = django_filters.ModelMultipleChoiceFilter(
+        label="Article Category",
+        field_name="category",
+        queryset=ArticleCategory.objects.all(),
+        help_text="Article Category",
         widget=DropdownSelectMultiple(),
     )
 
