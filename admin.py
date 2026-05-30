70af0fefc06e57a0b7b46a0dec3e5101b74afa0f
tougshire <tougshire@tougshire.com>
2026-05-30 10:22:59 -0400

    Add articlenote and articlenotesubject to admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
-from .models import Article, ArticleSnap, Role, ArticleStatus, Mamodel, MamodelCategory
+from .models import (
+    Article,
+    ArticleNote,
+    ArticleNoteSubject,
+    ArticleSnap,
+    Role,
+    ArticleStatus,
+    Mamodel,
+    MamodelCategory,
+)
 
 admin.site.register(Article)
 
@@ -13,3 +22,7 @@ admin.site.register(ArticleStatus)
 admin.site.register(Mamodel)
 
 admin.site.register(MamodelCategory)
+
+admin.site.register(ArticleNote)
+
+admin.site.register(ArticleNoteSubject)
